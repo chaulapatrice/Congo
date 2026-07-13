480709be4d594ccb5bbab14215a1429e6f00f795
Patrice Chaula <chaulapatrice@gmail.com>
2026-07-13 12:37:47 +0200

    fix: product port

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Running database migrations..."
 python manage.py migrate --noinput
 echo "Starting development server..."
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:$API_PORT
